056494ca25a309a309dff1aa8cebd7d421bc0ed2
Romulo Sanchez <sanchez.systems.24@gmail.com>
2026-03-15 18:53:57 -0600

    Add Telescope configuration and migrations

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\DomainServiceProvider::class,
     App\Providers\CommandBusServiceProvider::class,
+    App\Providers\DomainServiceProvider::class,
     App\Providers\QueryBusServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
